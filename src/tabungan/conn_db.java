212242f5109c32a99b0bec8bed1a30d4e2fa79e7
riogesta <riogesta.v@gmail.com>
2021-04-16 07:08:22 +0700

    initial commit

---
+++ src/tabungan/conn_db.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tabungan;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author riogesta
+ */
+public class conn_db {
+
+    private static Connection mysql_connection;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        // TODO code application logic here
+        if(mysql_connection == null){
+            try {
+                String db = "jdbc:mysql://localhost:3306/tabungan_us";
+                String user = "root"; 
+                String pw = "";
+                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+                mysql_connection = (Connection) DriverManager.getConnection(db,user,pw);
+                
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Koneksi database Gagal!\n"+e);
+            }
+        }
+    }
+    
+}
